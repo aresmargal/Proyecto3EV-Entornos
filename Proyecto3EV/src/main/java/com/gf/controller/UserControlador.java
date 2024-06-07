2bda8517b6928b745840a59740d77ac935be0151
aresmargal <151525545+aresmargal@users.noreply.github.com>
2024-06-07 21:08:58 +0200

    Controlador añadido

---
+++ Proyecto3EV/src/main/java/com/gf/controller/UserControlador.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gf.controller;
+
+import com.gf.entities.User;
+import com.gf.view.AddUser;
+import com.gf.view.SeleccionUser;
+import com.gf.logic.UserService;
+
+/**
+ *
+ * @author Ares
+ */
+public class UserControlador {
+
+    private UserService userService;
+
+    public UserControlador() {
+        this.userService = new UserService();
+    }
+    
+    public void mostrarAddUser(){
+        AddUser addUser = new AddUser(this);
+        addUser.setVisible(true);
+    }
+
+    public void mostrarSeleccionUser(){
+        SeleccionUser seleccionUser = new SeleccionUser(this);
+        seleccionUser.setVisible(true);
+    }
+    
+    public void addUser(String name, String DNI, int numBusca) {
+        User user = new User(name, DNI, 0);
+            userService.addUser(user);
+            // Mostrar mensaje de éxito o redirigir a una vista de éxito
+        
+    }
+}
