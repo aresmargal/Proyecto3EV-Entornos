60570e937bf19e7fc623c26692aa0269ef06b589
aresmargal <151525545+aresmargal@users.noreply.github.com>
2024-06-07 21:12:43 +0200

    Main añadido

---
+++ Proyecto3EV/src/main/java/com/gf/application/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gf.application;
+
+import com.gf.controller.UserControlador;
+
+/**
+ *
+ * @author Ares
+ */
+public class Main {
+    public static void main(String[] args) {
+        UserControlador controlador = new UserControlador();
+        controlador.mostrarSeleccionUser();
+    }
+}
